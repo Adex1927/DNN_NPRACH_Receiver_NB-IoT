2f880247d8dc3c4973b0c63dac310c26fc7df1db
Rishabh Tripathi <55353428+Adex1927@users.noreply.github.com>
2023-11-22 20:46:58 +0530

    add files

---
+++ DL_Receiver_C_Fixed/activations.c
@@ -0,0 +1,71 @@
+// All the required activation functions for the CNN
+
+#include "dl_nprach_head.h"
+
+// Function: relu()
+DATA** relu(int in_ch, int in_size, DATA **x)
+{
+    DATA **y = createMatrix_2D(in_ch, in_size);
+
+    for(int ch_i = 0; ch_i < in_ch; ch_i++)
+    {
+        for(int size_i = 0; size_i < in_size; size_i++)
+        {
+            if(x[ch_i][size_i] < 0)
+                y[ch_i][size_i] = 0;
+            else
+                y[ch_i][size_i] = x[ch_i][size_i];
+        }
+    }
+
+    // To free the memory for the output matrix of previous function
+    memory_free(x, in_ch);
+
+    return y;
+}
+
+// Function: sigmoid()
+DATA** sigmoid(int in_ch, int in_size, DATA **x, LUT_t *sig_lut)
+{
+    DATA **y = createMatrix_2D(in_ch, in_size);
+    DATA temp;
+
+    for(int ch_i = 0; ch_i < in_ch; ch_i++)
+    {
+        for(int size_i = 0; size_i < in_size; size_i++)
+        {
+            y[ch_i][size_i] = lookup_LUT(sig_lut, x[ch_i][size_i]);
+            // temp = exp(-1 * x[ch_i][size_i]);
+            // y[ch_i][size_i] = 1 / (1 + temp);
+        }
+    }
+
+    return y;
+}
+
+// Function: logsoftmax()
+DATA** logsoftmax(int in_ch, int in_size, DATA **x)
+{
+    DATA **y = createMatrix_2D(in_ch, in_size);
+    DATA sum = 0;
+
+    for(int ch_i = 0; ch_i < in_ch; ch_i++)
+    {
+        for(int size_i = 0; size_i < in_size; size_i++)
+        {
+            y[ch_i][size_i] = exp(x[ch_i][size_i]);
+            sum = sum + y[ch_i][size_i];
+        }
+    }
+
+    for(int ch_i = 0; ch_i < in_ch; ch_i++)
+    {
+        for(int size_i = 0; size_i < in_size; size_i++)
+        {
+            y[ch_i][size_i] = log10(y[ch_i][size_i] / sum);
+        }
+    }
+
+    return y;
+}
+
